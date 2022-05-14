2499dac36e3a7c1a2c4a02bf700f64f1f2bf4991
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-05-14 15:40:30 -0300

    Iniciando projeto

---
+++ src/main/java/com/example/AwereStuff/AwereStuffApplication.java
@@ -0,0 +1,13 @@
+package com.example.AwereStuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwereStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwereStuffApplication.class, args);
+	}
+
+}
